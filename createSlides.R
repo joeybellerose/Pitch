48c2181230a5d0fb657109f03c96a83cfa835fdb
Joey Bellerose <joey.bellerose@candoranalytics.io>
2019-01-21 15:46:51 -0500

    Added slide deck for meeting with JFTaylor

--- createSlides.R
+++ createSlides.R
@@ -2,4 +2,12 @@ library(rmarkdown)
 library(revealjs)
 
 # specify an option for incremental rendering
-render("OverviewSlides.Rmd", revealjs_presentation(incremental = TRUE, theme = "night", reveal_plugins = c("notes", "zoom"), self_contained = FALSE))
+render(
+  "JFTaylor.Rmd", 
+  revealjs_presentation(
+    incremental = FALSE, 
+    theme = "night", 
+    reveal_plugins = c("notes", "zoom"), 
+    self_contained = FALSE
+    )
+  )
